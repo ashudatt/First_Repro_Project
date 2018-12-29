b5641a21f154ecabd78b66d7a6300463afed597b
ashudatt <ashu_datt@rediffmail.com>
2018-12-29 23:47:38 +0530

    add code on remote repro

---
+++ FirstApplication_Gitlocal_remort/FirstApplication_Gitlocal_remort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication_Gitlocal_remort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World i am learing git ");
+            Console.ReadKey();
+        }
+    }
+}
